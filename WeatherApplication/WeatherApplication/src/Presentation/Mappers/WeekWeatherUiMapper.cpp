a75943cb6aedc5e0639d511b5dc44d46a79ef685
onweg <innokentiydiscord@yandex.ru>
2025-05-20 17:00:07 +0300

    убрал warning ошибку

--- WeatherApplication/WeatherApplication/src/Presentation/Mappers/WeekWeatherUiMapper.cpp
+++ WeatherApplication/WeatherApplication/src/Presentation/Mappers/WeekWeatherUiMapper.cpp
@@ -3,11 +3,9 @@
 
 void WeekWeatherUiMapper::toUiModel(const WeekWeatherData& dto, WeekWeatherUiModel* uiModel) {
     if (!uiModel) return;
-
     uiModel->setCity(QString::fromStdString(dto.city));
     uiModel->setMessageError(QString::fromStdString(dto.messageError));
     uiModel->clearDailyWeather();
-
     for (const auto& weatherDto : dto.dailyWeather) {
         auto* weatherUi = new WeatherUiModel(uiModel);
         WeatherUiMapper::toUiModel(weatherDto, weatherUi);
@@ -17,12 +15,11 @@ void WeekWeatherUiMapper::toUiModel(const WeekWeatherData& dto, WeekWeatherUiMod
 
 WeekWeatherData WeekWeatherUiMapper::fromUiModel(const WeekWeatherUiModel* uiModel) {
     if (!uiModel)  return {};
-
     WeekWeatherData data;
     data.city = uiModel->city().toStdString();
     data.messageError = uiModel->messageError().toStdString();
-
-    for (const auto* obj : uiModel->dailyWeather()) {
+    const QList<QObject*>& weatherList = uiModel->dailyWeather();
+    for (const auto* obj : weatherList) {
         const WeatherUiModel* weatherUi = qobject_cast<const WeatherUiModel*>(obj);
         if (weatherUi) {
             data.dailyWeather.push_back(WeatherUiMapper::fromUiModel(weatherUi));
