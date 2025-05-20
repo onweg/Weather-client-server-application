bb122180a9420dec4e6e8074806efcb337d24dcb
onweg <innokentiydiscord@yandex.ru>
2025-05-20 16:05:30 +0300

    декомпозировал код

--- WeatherApplication/WeatherApplication/src/Data/Repositories/Config/ConfigLoader.cpp
+++ WeatherApplication/WeatherApplication/src/Data/Repositories/Config/ConfigLoader.cpp
@@ -1,8 +1,8 @@
 #include "ConfigLoader.h"
 #include <QFile>
-#include <QJsonObject>
 #include <QJsonParseError>
 #include <QDebug>
+#include <QJsonDocument>
 
 #include "../../../Utils/AppConfigJsonConverter.h"
 
@@ -13,23 +13,45 @@ ConfigLoader::ConfigLoader()
 
 Result<AppConfig> ConfigLoader::load()
 {
-    QFile file(QString::fromStdString(FILE_PATH));
-    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-        qDebug() << "Не удалось открыть config.json файл";
-        return Result<AppConfig>::failure("Не удалось открыть config.json файл");
+    QByteArray data = readFile(FILE_PATH);
+    if (data.isEmpty()) {
+        return Result<AppConfig>::failure("Не удалось прочитать файл");
     }
-    QByteArray jsonData = file.readAll();
-    file.close();
-    QJsonParseError parseError;
-    QJsonDocument doc = QJsonDocument::fromJson(jsonData, &parseError);
-    if (parseError.error != QJsonParseError::NoError) {
-        qDebug() << "Ошибка парсинга json файла";
-        return Result<AppConfig>::failure("Ошибка парсинга json файла");
+    auto jsonResult = parseJson(data);
+    if (!jsonResult.isSuccess()) {
+        return Result<AppConfig>::failure(jsonResult.errorMessage());
     }
-    AppConfig appConfig;
-    if (AppConfigJsonConverter::fromJson(doc.object(), appConfig) == false) {
-        qDebug() << "Нет нужных полей в config.json";
-        return Result<AppConfig>::failure("Нет нужных полей в config.json");
+    AppConfig config;
+    if (!AppConfigJsonConverter::fromJson(jsonResult.value(), config)) {
+        return Result<AppConfig>::failure("Некорректный формат config.json");
     }
-    return Result<AppConfig>::success(appConfig);
+    return Result<AppConfig>::success(config);
 }
+
+QByteArray ConfigLoader::readFile(const QString &path)
+{
+    QFile file(path);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "Ошибка открытия файла:" << path;
+        return {};
+    }
+    QByteArray data = file.readAll();
+    file.close();
+    return data;
+}
+
+Result<QJsonObject> ConfigLoader::parseJson(const QByteArray &jsonData)
+{
+    QJsonParseError error;
+    QJsonDocument doc = QJsonDocument::fromJson(jsonData, &error);
+    if (error.error != QJsonParseError::NoError) {
+        qDebug() << "Ошибка парсинга JSON:" << error.errorString();
+        return Result<QJsonObject>::failure("Ошибка парсинга JSON");
+    }
+    if (!doc.isObject()) {
+        return Result<QJsonObject>::failure("JSON не является объектом");
+    }
+    return Result<QJsonObject>::success(doc.object());
+}
+
+
--- WeatherApplication/WeatherApplication/src/Data/Repositories/Config/ConfigLoader.h
+++ WeatherApplication/WeatherApplication/src/Data/Repositories/Config/ConfigLoader.h
@@ -2,13 +2,17 @@
 #define CONFIGLOADER_H
 
 #include "../../../Domain/Interfaces/Config/IConfigLoader.h"
-
+#include <QByteArray>
+#include <QJsonObject>
 #define FILE_PATH "/usr/share/ru.auroraos.WeatherApplication/config/config.json"
 
 class ConfigLoader : public IConfigLoader {
 public:
     explicit ConfigLoader();
     Result<AppConfig> load() override;
+private:
+    QByteArray readFile(const QString& path);
+    Result<QJsonObject> parseJson(const QByteArray& jsonData);
 };
 
 #endif // CONFIGLOADER_H
