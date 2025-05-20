be082e12e3c681554d7d62ce4c23e8df28a8f720
onweg <innokentiydiscord@yandex.ru>
2025-05-20 16:15:21 +0300

    декомпозировал код

--- WeatherApplication/WeatherApplication/src/Data/Repositories/Database/SqliteWeatherHistoryRepository.cpp
+++ WeatherApplication/WeatherApplication/src/Data/Repositories/Database/SqliteWeatherHistoryRepository.cpp
@@ -18,37 +18,50 @@ SqliteWeatherHistoryRepository::SqliteWeatherHistoryRepository(std::shared_ptr<I
 std::vector<WeatherHistoryItem> SqliteWeatherHistoryRepository::getAll() {
     std::vector<WeatherHistoryItem> results;
     QSqlQuery query("SELECT id, username, timestamp, city, date FROM weather_data");
-
-    if (!query.exec()) {
-        qDebug() << "Ошибка при получении данных:" << query.lastError();
-        return results;
-    }
-
+    if (!executeQuery(query, "Ошибка при получении данных")) return results;
     while (query.next()) {
-        WeatherHistoryItem row;
-        row.id = query.value("id").toInt();
-        row.username = query.value("username").toString().toStdString();
-        row.city = query.value("city").toString().toStdString();
-        row.date = query.value("date").toString().toStdString();
-
-        QDateTime timestamp = query.value("timestamp").toDateTime();
-        row.timestamp = std::chrono::system_clock::from_time_t(static_cast<std::time_t>(timestamp.toMSecsSinceEpoch() / 1000));
-        results.push_back(row);
+        results.push_back(extractWeatherHistoryItem(query));
     }
-
     return results;
 }
 
 void SqliteWeatherHistoryRepository::save(const std::string &city, const std::string &date) {
     QSqlQuery query;
-    query.prepare("INSERT INTO weather_data (username, city, date) "
-                      "VALUES (:username, :city, :date)");
+    prepareInsertStatement(query);
+    bindInsertValues(query, city, date);
+    if (!executeQuery(query, "Ошибка при вставке")) return;
+}
 
+WeatherHistoryItem SqliteWeatherHistoryRepository::extractWeatherHistoryItem(const QSqlQuery &query) {
+    WeatherHistoryItem item;
+    item.id = query.value("id").toInt();
+    item.username = query.value("username").toString().toStdString();
+    item.city = query.value("city").toString().toStdString();
+    item.date = query.value("date").toString().toStdString();
+    item.timestamp = convertToTimePoint(query.value("timestamp").toDateTime());
+    return item;
+}
+
+std::chrono::system_clock::time_point SqliteWeatherHistoryRepository::convertToTimePoint(const QDateTime &dateTime) {
+    return std::chrono::system_clock::from_time_t(static_cast<std::time_t>(dateTime.toMSecsSinceEpoch() / 1000));
+}
+
+bool SqliteWeatherHistoryRepository::executeQuery(QSqlQuery &query, const char *errorMessage) {
+    if (!query.exec()) {
+        qDebug() << errorMessage << ":" << query.lastError();
+        return false;
+    }
+    return true;
+}
+
+void SqliteWeatherHistoryRepository::prepareInsertStatement(QSqlQuery &query) {
+    bool ok = query.prepare("INSERT INTO weather_data (username, city, date) "
+                                "VALUES (:username, :city, :date)");
+    Q_ASSERT(ok);
+}
+
+void SqliteWeatherHistoryRepository::bindInsertValues(QSqlQuery &query, const std::string &city, const std::string &date) {
     query.bindValue(":username", QString::fromStdString(sharedState_->getUsername()));
     query.bindValue(":city", QString::fromStdString(city));
     query.bindValue(":date", QString::fromStdString(date));
-
-    if (!query.exec()) {
-        qDebug() << "Ошибка при вставке:" << query.lastError();
-    }
 }
--- WeatherApplication/WeatherApplication/src/Data/Repositories/Database/SqliteWeatherHistoryRepository.h
+++ WeatherApplication/WeatherApplication/src/Data/Repositories/Database/SqliteWeatherHistoryRepository.h
@@ -2,6 +2,7 @@
 #define SQLITEWEATHERHISTORYREPOSITORY_H
 
 #include <QSqlDatabase>
+#include <QDateTime>
 #include <memory>
 #include "../../../Domain/Interfaces/Database/IWeatherHistoryRepository.h"
 #include "../../../Domain/Interfaces/Database/IWeatherDatabaseInitializer.h"
@@ -16,6 +17,12 @@ public:
 private:
     QSqlDatabase db_;
     std::shared_ptr<ISharedState> sharedState_;
+
+    WeatherHistoryItem extractWeatherHistoryItem(const QSqlQuery& query);
+    std::chrono::system_clock::time_point convertToTimePoint(const QDateTime& dateTime);
+    bool executeQuery(QSqlQuery& query, const char* errorMessage);
+    void prepareInsertStatement(QSqlQuery& query);
+    void bindInsertValues(QSqlQuery& query, const std::string& city, const std::string& date);
 };
 
 #endif // SQLITEWEATHERHISTORYREPOSITORY_H
